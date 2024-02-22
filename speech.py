04d299ed8fa4e050bd376071357d93a3db4ff3ac
laura-gonzalez19 <77318872+laura-gonzalez19@users.noreply.github.com>
2024-02-22 14:35:48 -0800

    commiting and pushing

---
+++ speech.py
@@ -0,0 +1,11 @@
+### Laura's Speech file
+{
+    "config": {
+        "encoding": "LINEAR16",
+        "sampleRateHertz": 16000,
+        "languageCode": "en-US",
+    },
+    "audio": {
+        "uri": "gs://bucket-name/path_to_audio_file"
+    }
+}
