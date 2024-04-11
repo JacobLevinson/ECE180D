a634695f30a4b63abbb78f9f956c85143ec7e55a
EscobarWilliam <williamj915@gmail.com>
2024-04-10 21:32:03 -0700

    Pushing code

--- LEDArray/6LEDArray.ino
+++ LEDArray/6LEDArray.ino
@@ -1,8 +1,14 @@
 #include <FastLED.h>
-#include <ArduinoJson.h>
 #include <PubSubClient.h>
 #include <WiFiNINA.h>
-#include <Arduino_JSON.h>
+
+const char* ssid = "INSERT_WIFI_NETWORK_HERE";
+const char* password = "ENTER_PASSWORD_HERE";
+const char mqtt_server[] = "mqtt.eclipseprojects.io";
+const char subscribeTopic[] = "ece180d/team3/reverseabomb/ledcontroller";
+
+WiFiClient wifiClient;
+PubSubClient mqtt(wifiClient);
 
 // Number of LED arrays
 #define NUM_LED_ARRAYS 6
@@ -13,18 +19,9 @@
 // Specify the data pins for each LED array
 const int DATA_PINS[NUM_LED_ARRAYS] = {3, 4, 5, 6, 7, 8};
 
-StaticJsonDocument<256> doc;
 
 #define TOKEN ""
 #define DEVICEID ""
-
-const char* ssid = "INSERT_WIFI_NETWORK_HERE";
-const char* password = "ENTER_PASSWORD_HERE";
-const char mqtt_server[] = "mqtt.eclipseprojects.io";
-const char subscribeTopic[] = "ece180d/team3/reverseabomb/ledcontroller";
-
-WiFiClient wifiClient;
-PubSubClient mqtt(wifiClient);
 
 CRGB leds[NUM_LED_ARRAYS][NUM_LEDS_PER_ARRAY]; //2D array to help control the 6 LED arrays 
 
@@ -66,13 +63,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
 
-  Serial.print("Message:");
+  Serial.print("LED State:");
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
   Serial.println();
 
-  deserializeJson(doc, payload); //read the JSON file
+  
 
   //now with the read data, update the LEDS for the gameboard
   for (int stripIndex = 0; stripIndex < NUM_LED_ARRAYS; stripIndex++) 
