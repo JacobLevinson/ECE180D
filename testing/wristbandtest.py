2c60b5b80b874c6f3eaae7890be17af6dd951e65
Jacob Levinson <96364327+JacobLevinson@users.noreply.github.com>
2024-05-30 17:37:36 -0700

    Fixed wristabnd code to not stall w/ no serial
    Also added wristband test code

--- testing/wristbandtest.py
+++ testing/wristbandtest.py
@@ -17,3 +17,31 @@ wristband2_topic = "ece180d/team3/reverseabomb/wristband2"
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
     client.subscribe(wristband1_topic, qos=1)
+
+
+def on_message(client, userdata, msg):
+    # Process incoming message
+    topic = msg.topic
+    wristband_id = topic.split('/')[-1]  # Extract wristband ID from topic
+    message_content = str(msg.payload.decode(
+        "utf-8")).upper()  # Assuming payload is text
+    match message_content:
+        case "SLAP1":
+            print(f"SLAP received from wristband 1")
+        case "SLAP2":
+            print(f"SLAP received from wristband 2")
+
+
+def main():
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect_async(mqtt_server)
+    client.loop_start()
+    while True:
+        time.sleep(1)
+
+
+
+if __name__ == "__main__":
+    main()
